94000acd755c88c5dcc83ff10a0756399b29ca96
Bian Lee <bianlee2004@gmail.com>
2024-04-19 22:08:57 -0700

    .

---
+++ stores/authStore.js
@@ -0,0 +1,10 @@
+import create from "zustand";
+
+const useAuthStore = create((set) => ({
+  isAuthenticated: false,
+  user: null,
+  setAuth: (user) => set({ isAuthenticated: !!user, user }),
+  setIsAuthenticated: (value) => set({ setIsAuthenticated: value }),
+}));
+
+export default useAuthStore;
