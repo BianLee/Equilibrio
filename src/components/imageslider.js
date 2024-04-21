b2d7718b7017ef6c0d2af23ee912060ff1e58950
Ritvik Irigireddy <ririgireddy1@gmail.com>
2024-04-21 06:25:22 -0700

    pose names

--- src/components/imageslider.js
+++ src/components/imageslider.js
@@ -17,6 +17,7 @@ function ImageSlider() {
     <div className="bg-white p-4 mt-4 rounded-lg shadow h-auto flex flex-col">
       <Slider {...settings}>
         <div>
+          <p className="text-center text-black text-2xl font-bold">Cobra</p>
           <img
             src="./cobra.png"
             alt="Image 1"
@@ -25,6 +26,7 @@ function ImageSlider() {
           />
         </div>
         <div>
+          <p className="text-center text-black text-2xl font-bold">Dog</p>
           <img
             src="dog.png"
             alt="Image 2"
@@ -33,6 +35,7 @@ function ImageSlider() {
           />
         </div>
         <div>
+          <p className="text-center text-black text-2xl font-bold">Tree</p>
           <img
             src="tree.jpg"
             alt="Image 3"
@@ -41,6 +44,7 @@ function ImageSlider() {
           />
         </div>
         <div>
+          <p className="text-center text-black text-2xl font-bold">Warrior</p>
           <img
             src="warrior.jpg"
             alt="Image 4"
@@ -49,6 +53,7 @@ function ImageSlider() {
           />
         </div>
         <div>
+          <p className="text-center text-black text-2xl font-bold">Chair</p>
           <img
             src="chair.jpg"
             alt="Image 5"
