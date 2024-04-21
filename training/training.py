28b731a2c9221301dfb79cc11123808e01b94532
paulshimdev <paulshimdev@gmail.com>
2024-04-20 23:27:12 -0700

    very ugly python training code

--- training/training.py
+++ training/training.py
@@ -1,27 +1,77 @@
+# import csv
+# import cv2
+# import itertools
+# import numpy as np
+# import pandas as pd
+import os
+import sys
+import json
+# import tempfile
+# import tqdm
+
+# from matplotlib import pyplot as plt
+# from matplotlib.collections import LineCollection
+
 import tensorflow as tf
-from tfjs_models import posenet
+from pathlib import Path
+# import tensorflow_hub as hub
+# from tensorflow import keras
 
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-image_paths = "./yoga_poses/train/chairgirl1_chair070.jpg"
+pose_sample_rpi_path = os.path.join(os.getcwd(), 'examples/lite/examples/pose_estimation/raspberry_pi')
+sys.path.append(pose_sample_rpi_path)
 
-def load_and_preprocess_image(image_path):
-    img = tf.io.read_file(image_path)
-    img = tf.image.decode_jpeg(img, channels=3)
-    img = tf.image.resize(img, [224, 224])  # Resize to the input size required by PoseNet
-    img = tf.cast(img, tf.float32) / 255.0  # Normalize pixel values
-    return img
+from data import BodyPart
+from ml import Movenet
+movenet = Movenet('movenet_thunder')
 
-# Assuming 'image_paths' is a list of file paths to your images
-dataset = tf.data.Dataset.from_tensor_slices(image_paths)
-dataset = dataset.map(load_and_preprocess_image)
+def detect(input_tensor, inference_count=3):
+    """Runs detection on an input image.
 
-dataset = tf.data.Dataset.from_tensor_slices(image_paths)
-dataset = dataset.map(load_and_preprocess_image)
+    Args:
+    input_tensor: A [height, width, 3] Tensor of type tf.float32.
+        Note that height and width can be anything since the image will be
+        immediately resized according to the needs of the model within this
+        function.
+    inference_count: Number of times the model should run repeatly on the
+        same input image to improve detection accuracy.
 
-# Dummy example for extracting features using a loaded PoseNet model
-poses = []
-for image in dataset:
-    pose = posenet.estimate_single_pose(image)  # This function needs to be defined or adapted
-    poses.append(pose)
+    Returns:
+    A Person entity detected by the MoveNet.SinglePose.
+    """
+    image_height, image_width, channel = input_tensor.shape
 
-print(poses)
+    # Detect pose using the full input image
+    movenet.detect(input_tensor.numpy(), reset_crop_region=True)
+
+    # Repeatedly using previous detection result to identify the region of
+    # interest and only croping that region to improve detection accuracy
+    for _ in range(inference_count - 1):
+        person = movenet.detect(input_tensor.numpy(), 
+                            reset_crop_region=False)
+
+    return person
+
+ypt_dir = Path("./training/yoga_poses/train/chair")
+all_poses = dict()
+
+for yoga_pose in ypt_dir.iterdir():
+    image = tf.io.read_file(str(yoga_pose.absolute()))
+    image = tf.io.decode_png(image)
+
+    person = detect(image)
+    person_coords = dict()
+    for keypoint in person.keypoints:
+        person_coords[keypoint.body_part.name] = dict()
+        person_coords[keypoint.body_part.name]["coordinates"] = dict()
+        person_coords[keypoint.body_part.name]["coordinates"]["x"] = str(keypoint.coordinate.x)
+        person_coords[keypoint.body_part.name]["coordinates"]["y"] = str(keypoint.coordinate.y)
+        person_coords[keypoint.body_part.name]["score"] = str(keypoint.score)
+
+    all_poses[yoga_pose.name] = person_coords
+
+print(all_poses)
+write_file = Path("./training/pose_data.txt").open('w')
+write_file.write(str(json.dumps(all_poses)))
